d2790f8c84cdc210f680693f896c73b79f74c714
nathalyabarbosa <130375787+nathalyabarbosa@users.noreply.github.com>
2024-08-27 07:48:13 -0300

    555555

---
+++ main.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
