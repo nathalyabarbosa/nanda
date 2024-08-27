60cb9f4ceb1f54fb5aa4d5bcc374f4197695fe3b
nathalyabarbosa <130375787+nathalyabarbosa@users.noreply.github.com>
2024-08-27 10:56:08 -0300

    5555

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ const Pergunta =[
     {
         enunciado: "Pergunta 1",
         alternativas: ["aternativa1","alternativa2","alternativa3"]
-    }
+    },
     {
         enunciado: "Pergunta 2",
         alternativas: ["aternativa1","alternativa2","alternativa3"]
-    }
+    },
     {
         enunciado: "Pergunta 3",
         alternativas: ["aternativa1","alternativa2","alternativa3"]
     
-    }
+    },
     {
         enunciado: "Pergunta 4",
         alternativas: ["aternativa1","alternativa2","alternativa3"]
     
-    }
+    },
 ]
